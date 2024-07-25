9bd4bd9323a6ded3118a9c499184609a6a04468c
Samgibs <117844998+Samgibs@users.noreply.github.com>
2024-07-25 20:42:27 +0300

    Update settings.py
    allowed hosts

--- the_market/the_market/settings.py
+++ the_market/the_market/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-m3l1j0bw@-f$67akfccx&r)&l^zun4$zor&reivx+sfd-tcq-(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['157.230.62.218','localhost']
+ALLOWED_HOSTS = ['157.230.62.218','127.0.0.1:8000']
 
 
 # Application definition
